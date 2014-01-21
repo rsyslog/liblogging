6d4ffb23ff513d162e4c61cd171ee04c2452fdf9
Rainer Gerhards <rgerhards@adiscon.com>
2014-01-21 12:37:00 +0100

    set license to 2-clause BSD for easier adoption; some doc
    Now provides the bare essentials. This permits us to begin using it
    while we can extend the functionality towards our first milestone.

--- stdlog/libstdlog.h
+++ stdlog/libstdlog.h
@@ -1,6 +1,28 @@
 /* The stdlog main header file
- * Copyright (C) 2014 by Rainer Gerhards and Adiscon GmbH
- * Released under the ASL 2.0
+ *
+ * Copyright (C) 2014 Adiscon GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ADISCON AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL ADISCON OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef LIBSTDLOG_H_INCLUDED
--- stdlog/stdlog.c
+++ stdlog/stdlog.c
@@ -1,8 +1,30 @@
 /* The stdlog main file
- * Copyright (C) 2014 by Rainer Gerhards and Adiscon GmbH
- * This is currently a small chim, which enables us to inject the full
+ * This is currently a small shim, which enables us to inject the full
  * functionality at a later point in time.
- * Released under the ASL 2.0
+ *
+ * Copyright (C) 2014 Adiscon GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ADISCON AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL ADISCON OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 #include "config.h"
 #include <stdio.h>
@@ -10,7 +32,11 @@
 #include <syslog.h>
 #include "libstdlog.h"
 
-/* returns 0 on success or a standard (negative) error code */
+/* Log a message to the specified channel. If channel is NULL,
+ * use the default channel (which always exists).
+ * Returns 0 on success or a standard (negative) error code.
+ * Otherwise the semantics are equivalent to syslog().
+ */
 int
 stdlog_log(stdlog_channel_t __attribute((unused)) channel,
 	const int severity, const char *fmt, ...)
